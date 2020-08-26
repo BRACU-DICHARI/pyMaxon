2692c287df8266f90b39286f68ac6f0fde70436c
Riccardo <solidkind@gmail.com>
2020-08-26 21:50:42 +0200

    - Init version

---
+++ pyMaxon/pyMaxon.py
@@ -0,0 +1,6 @@
+import ctypes
+
+
+class Maxon():
+
+    pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyMaxon',
+    version='0.0.1',
+    packages=['pyMaxon'],
+    url='',
+    license='MIT',
+    author='Riccardo',
+    author_email='solidkind@gmail.com',
+    description='Wrapper for Maxon Library'
+)
